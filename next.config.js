6861d6fe979cabec03beeaae897f1fe33d842af4
Diego Bassani de Souza <diego.bassani@soluevo.com.br>
2023-01-26 13:23:44 -0300

    Test

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = withPWA({
+  // next.js config
+})
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+import { Sidebar } from 'components/Sidebar/index'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Container>
+      <Sidebar />
+    </S.Container>
+  </S.Wrapper>
+)
+
+export default Main
